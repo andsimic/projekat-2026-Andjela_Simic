4d1864dfaa0195ea353e58f3796310ad4971a971
Ucenik <Ucenik@DESKTOP-A6MV7AL>
2026-03-18 10:26:21 +0100

    login v0.11

--- Login.cs
+++ Login.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace projekat_2026_Andjela_Simic
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Andjela Simic
+            //drugi red
         }
     }
 }
